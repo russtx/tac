c312721591a46f104793222d88731ee60264006c
Russ <Russ@CodeJunky>
2015-06-27 09:21:01 -0400

    added map and links

--- wp-content/themes/custom-built-for-tac/homepage.php
+++ wp-content/themes/custom-built-for-tac/homepage.php
@@ -12,23 +12,25 @@ get_header(); ?>
         <div class="homepageInfo">
             <ul>
                 <li><a href="tel:8044238400"><i class="fa fa-phone"></i>804-423-8400</a></li>
-                <li><a href="#"><i class="fa fa-map-marker"></i>11201 Midlothian Turnpike<br /> &nbsp; &nbsp; Midlothian, VA 23235</a></li>
-                <li><a href="#"><i class="fa fa-envelope"></i>Contact Us</a></li>
+                <li><a href="http://localhost/totalacccenter/?page_id=92"><i class="fa fa-map-marker"></i>11201 Midlothian Turnpike<br /> &nbsp; &nbsp; Midlothian, VA 23235</a></li>
+                <li><a href="http://localhost/totalacccenter/?page_id=49"><i class="fa fa-envelope"></i>Contact Us</a></li>
             </ul>    
         </div>
 
-        <div class="slidingIcons flex-container">
-            <ul>
-                <li class="iconBox icon1 flex1"><img src='http://localhost/totalacccenter/wp-content/uploads/2015/05/tools_256px.png'></li>
-                <li class="iconBox icon2 flex1"><img src='http://localhost/totalacccenter/wp-content/uploads/2015/05/tools_256px.png'></li>
-                <li class="iconBox icon3 flex1"><img src='http://localhost/totalacccenter/wp-content/uploads/2015/05/tools_256px.png'></li>
-                <li class="iconBox icon4 flex1"><img src='http://localhost/totalacccenter/wp-content/uploads/2015/05/tools_256px.png'></li>
-                <li class="iconBox icon5 flex1"><img src='http://localhost/totalacccenter/wp-content/uploads/2015/05/tools_256px.png'></li>
-            </ul>
-        </div>
+        
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+                    
+          <div class="slidingIcons flex-container">
+            <ul>
+                <li class="iconBox icon1 flex1"><a href="#"><button class="homeButton"><img src='http://localhost/totalacccenter/wp-content/uploads/2015/06/Interior-Icon_50.png'><h4>Interior</h4></button></a></li>
+                <li class="iconBox icon2 flex1"><a href="#"><button class="homeButton"><img src='http://localhost/totalacccenter/wp-content/uploads/2015/06/Exterior_50.png'><h4>Exterior</h4></button></a></li>
+                <li class="iconBox icon3 flex1"><a href="#"><button class="homeButton"><img src='http://localhost/totalacccenter/wp-content/uploads/2015/06/12-Volt_50.png'><h4>12 Volt</h4></button></a></li>
+                <li class="iconBox icon4 flex1"><a href="#"><button class="homeButton"><img src='http://localhost/totalacccenter/wp-content/uploads/2015/06/TowPackage_50.png'><h4>Tow Package</h4></button></a></li>
+                <li class="iconBox icon5 flex1"><a href="#"><button class="homeButton"><img src='http://localhost/totalacccenter/wp-content/uploads/2015/06/SprayLiner_50.png'><h4>Spray Liner</h4></button></a></li>
+            </ul>
+        </div>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
